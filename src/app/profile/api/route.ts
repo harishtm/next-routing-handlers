b9bf3d46ba2ca27794b98891c028f2a3673cfe0b
Harish T M <harishtm77@gmail.com>
2024-09-25 15:03:03 +0530

    headers in route handler

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,18 @@
-export async function GET () {
-    return new Response("Profile API")
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function GET (request: NextRequest) {
+    // Two ways of retrieving the header
+    // Approach one
+    const requestHeader = new Headers(request.headers)
+    console.log("Approach One -> ", requestHeader.get('Authorization'))
+
+    // Approach two
+    const headerList = headers()
+    console.log("Approach Two -> ", headerList.get('Authorization'))
+    return new Response("<h1>Profile API</h1>", {
+        headers: {
+            "Content-Type": "text/html"
+        }
+    })
 }
