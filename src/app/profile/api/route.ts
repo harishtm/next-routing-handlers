e179c2fcde6c8ddc323209badda427f1865e1d78
Harish T M <harishtm77@gmail.com>
2024-09-26 09:47:14 +0530

    Cookies in route handling

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,4 +1,4 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET (request: NextRequest) {
@@ -10,9 +10,17 @@ export async function GET (request: NextRequest) {
     // Approach two
     const headerList = headers()
     console.log("Approach Two -> ", headerList.get('Authorization'))
+
+    // Cookie set/get can be achived in two ways one in response header
+    cookies().set("resultPerPage", "20");
+    const theme = request.cookies.get("theme")
+    const perPage = request.cookies.get("resultPerPage")
+    console.log("retrieving Cookie", theme, perPage)
+
     return new Response("<h1>Profile API</h1>", {
         headers: {
-            "Content-Type": "text/html"
+            "Content-Type": "text/html",
+            "Set-Cookie": "theme=dark"
         }
     })
 }
